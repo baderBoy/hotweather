a75316704d7ab3fc14ac5bb1681799ee06a06186
Tiger <1534750205@qq.com>
2016-09-29 16:22:18 +0800

    First commit

---
+++ app/src/main/java/com/example/hotweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hotweather;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends /*AppCompat*/Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
